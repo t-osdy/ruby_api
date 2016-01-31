a71ec2fe66fac12d15c61e43acc0ba8980cda59e
tosada <tomoya.a.osada@gmail.com>
2016-02-01 01:41:52 +0900

    change ruby

--- atend/atend_api.rb
+++ atend/atend_api.rb
@@ -1,12 +1,12 @@
 require 'rexml/document'
 require 'open-uri'
 
-uri = "http://api.atnd.org/events/?"
-path = "keyword=solr"
+uri = "http://api.atnd.org/events/"
+path = "?keyword=ruby"
 result = open(uri+path)
 
 doc = REXML::Document.new(result)
 
-doc.elements.each('hash/events/event[1]/title') do |element|
+doc.elements.each('hash/events/event[1]/description') do |element|
   puts element.text
 end
